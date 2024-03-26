86e9eeaffd11cf0a2d18de6033ca5c02115450ca
Austin Miljour <austinmiljour@gmail.com>
2024-03-26 12:39:23 -0700

    Started Project

---
+++ game-nexus-market/src/main/java/com/austinmiljour/gamenexusmarket/GameNexusMarketApplication.java
@@ -0,0 +1,13 @@
+package com.austinmiljour.gamenexusmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameNexusMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameNexusMarketApplication.class, args);
+	}
+
+}
---
+++ game-nexus-market/src/test/java/com/austinmiljour/gamenexusmarket/GameNexusMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.austinmiljour.gamenexusmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameNexusMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
