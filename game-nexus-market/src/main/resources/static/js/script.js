5562c33bc79075bf1f4a541ffad6c87f47a76828
Austin Miljour <austinmiljour@gmail.com>
2024-04-29 18:33:28 -0700

    Added Script for Mobile Nav Bar

--- game-nexus-market/src/main/resources/static/js/script.js
+++ game-nexus-market/src/main/resources/static/js/script.js
@@ -1,8 +1,15 @@
-// Initialization for ES Users
-import {
-  Collapse,
-  Dropdown,
-  initTWE,
-} from "tw-elements";
 
-initTWE({ Collapse, Dropdown });
+    
+document.addEventListener('DOMContentLoaded', function () {
+    // Function to toggle menu visibility
+    function toggleMenu() {
+        var dropdown = document.getElementById('mobile-menu');
+        dropdown.classList.toggle('hidden');
+    }
+
+    // Attach event listener to mobile menu button
+    document.getElementById('mobile-menu-button').addEventListener('click', toggleMenu);
+
+    // Attach the same event listener to the h2 tag
+    document.getElementById('mobile-menu-trigger').addEventListener('click', toggleMenu);
+});
